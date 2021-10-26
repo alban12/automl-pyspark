3b16914bfb75aed8db5f9328623dd7e2eb47c0fa
Alban Tiacoh <alban.kdf@gmail.com>
2021-10-26 17:03:50 +0200

    Add the transformations for feature generation

---
+++ automl_iasd/feature_engineering/GroupByThenTransformer.py
@@ -0,0 +1,61 @@
+from pyspark.sql.functions import when
+from pyspark import keyword_only
+from pyspark.ml.pipeline import Transformer
+from pyspark.ml.param.shared import HasInputCol, HasInputCols, HasOutputCol, HasOutputCols, Param
+
+class GroupByThenTransformer(Transformer, HasInputCols, HasOutputCols):
+    """Limit Cardinality of a column."""
+
+    @keyword_only
+    def __init__(self, inputCols=None, outputCols=None, n=None):
+        """Initialize."""
+        super(GroupByThenTransformer, self).__init__()
+        self.n = Param(self, "n", "Cardinality upper limit.")
+        self._setDefault(n=25)
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    @keyword_only
+    def setParams(self, inputCols=None, outputCols=None, n=None):
+        """Get params."""
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
+
+    def setN(self, value):
+        """Set cardinality limit."""
+        return self._set(n=value)
+
+    def getN(self):
+        """Get cardinality limit."""
+        return self.getOrDefault(self.n)
+
+    def _transform(self, dataframe):
+        """Do transformation."""
+        out_cols = self.getOutputCols()
+        in_cols = dataframe[self.getInputCols()]
+
+
+        print(self.getInputCols())
+
+        print(in_cols)
+
+        in_cols = self.getInputCols()
+
+        # Min 
+        grouped_by_then_df = dataframe.groupBy(in_cols[0]).min(in_cols[1]).withColumnRenamed(f"min({in_cols[1]})",f"group_by_{in_cols[0]}_{out_cols[0]}_{in_cols[1]}")
+        dataframe = dataframe.join(grouped_by_then_df, [in_cols[0]])
+
+        # Max 
+        grouped_by_then_df = dataframe.groupBy(in_cols[0]).max(numeric_column).withColumnRenamed(f"max({in_cols[1]})",f"group_by_{in_cols[0]}_{out_cols[1]}_{in_cols[1]}")
+        dataframe = dataframe.join(grouped_by_then_df, [in_cols[0]])
+
+        # Avg 
+        grouped_by_then_df = dataframe.groupBy(in_cols[0]).avg(numeric_column).withColumnRenamed(f"avg({in_cols[1]})",f"group_by_{in_cols[0]}_{out_cols[2]}_{in_cols[1]}")
+        dataframe = dataframe.join(grouped_by_then_df, [in_cols[0]])
+
+        # Count 
+        if f"group_by_{categorical_column}_count" not in dataframe.schema.names:
+            grouped_by_then_df = dataframe.groupBy(in_cols[0]).count().withColumnRenamed(f"count",f"group_by_{in_cols[0]}_{out_cols[3]}")
+            dataframe = dataframe.join(grouped_by_then_df, [in_cols[0]])
+
+        return dataframe, assembler
--- automl_iasd/feature_engineering/transformations.py
+++ automl_iasd/feature_engineering/transformations.py
@@ -2,27 +2,31 @@ from pyspark.ml.feature import QuantileDiscretizer, PolynomialExpansion
 from pyspark.sql.functions import col
 from pyspark.sql.functions import when
 from pyspark.ml.feature import VectorAssembler
+from feature_engineering.util import get_init_param_from_meta_learning
+from automl_iasd.feature_engineering.transformers import ArithmeticTransformer, GroupByThenTransformer
+
+
 
 def apply_discretization(dataframe, columns):
 	"""Apply discretization."""
 	output_columns = ["discretized_"+col for col in columns]
 	discretizer = QuantileDiscretizer(numBuckets=10, inputCols=columns, outputCols=output_columns)
 	dataframe = discretizer.fit(dataframe).transform(dataframe)
-	return dataframe#, output_columns
+	return dataframe, discretizer#, output_columns
 
 def apply_polynomial_expansion(dataframe, columns):
 	"""Apply polynomial expansion."""
 	#output_columns = ["polynomial_expanded_"+col for col in columns]
 	output_columns = "_".join(columns)
-	assembler = VectorAssembler(
+	polynomial_assembler = VectorAssembler(
 	    inputCols=columns,
 	    outputCol="vectorized_"+output_columns)
 
-	dataframe = assembler.transform(dataframe)
+	dataframe = polynomial_assembler.transform(dataframe)
 
 	polyExpansion = PolynomialExpansion(degree=3, inputCol="vectorized_"+output_columns, outputCol="feature_polynomially_expanded_"+output_columns)
 	dataframe = polyExpansion.transform(dataframe)
-	return dataframe#, output_columns
+	return dataframe, polynomial_assembler, polyExpansion#, output_columns
 
 
 def apply_binary_transformation(dataframe, columns):
@@ -31,41 +35,49 @@ def apply_binary_transformation(dataframe, columns):
 	column_name = "_".join(columns)
 	column_name_reverse = "_".join(columns[::-1])
 
-	#Addition
-	dataframe = dataframe.withColumn(f"sum_{column_name}", col(f"{columns[0]}") + col(f"{columns[1]}"))
+	outputCols = "_".join(columns)
 
-	#Substraction
-	dataframe = dataframe.withColumn(f"difference_{column_name}", col(f"{columns[0]}") - col(f"{columns[1]}"))
-	dataframe = dataframe.withColumn(f"difference_{column_name_reverse}", col(f"{columns[1]}") - col(f"{columns[0]}"))
+	arithmetic_transformer = ArithmeticTransformer(inputCols=columns, outputCols=[f"sum_{column_name}", f"difference_{column_name}", f"difference_{column_name_reverse}", f"multiplied_{column_name}", f"division_{column_name}", f"division_{column_name_reverse}"])
 
-	#Mulitplication
-	dataframe = dataframe.withColumn(f"multiplied_{column_name}", col(f"{columns[0]}") * col(f"{columns[1]}"))
-	
-	#Division
-	dataframe = dataframe.withColumn(f"division_{column_name}", when(col(f"{columns[1]}") != 0, col(f"{columns[0]}") / col(f"{columns[1]}")).otherwise(0))
-	dataframe = dataframe.withColumn(f"division_{column_name_reverse}", when(col(f"{columns[0]}") != 0, col(f"{columns[1]}") / col(f"{columns[0]}")).otherwise(0))
+	dataframe = arithmetic_transformer.transform(dataframe)
 
-	return dataframe
+	return dataframe, arithmetic_transformer
 
 
 def apply_group_by_then_transformation(dataframe, categorical_column, numeric_column):
 	"""Apply all unary transformations on columns passed."""
 
 	# Min 
-	grouped_by_then_df = dataframe.groupBy(categorical_column).min(numeric_column).withColumnRenamed(f"min({numeric_column})",f"group_by_{categorical_column}_min_{numeric_column}")
-	dataframe = dataframe.join(grouped_by_then_df, [categorical_column])
+	#grouped_by_then_df = dataframe.groupBy(categorical_column).min(numeric_column).withColumnRenamed(f"min({numeric_column})",f"group_by_{categorical_column}_min_{numeric_column}")
+	#dataframe = dataframe.join(grouped_by_then_df, [categorical_column])
 
 	# Max 
-	grouped_by_then_df = dataframe.groupBy(categorical_column).max(numeric_column).withColumnRenamed(f"max({numeric_column})",f"group_by_{categorical_column}_max_{numeric_column}")
-	dataframe = dataframe.join(grouped_by_then_df, [categorical_column])
+	#grouped_by_then_df = dataframe.groupBy(categorical_column).max(numeric_column).withColumnRenamed(f"max({numeric_column})",f"group_by_{categorical_column}_max_{numeric_column}")
+	#dataframe = dataframe.join(grouped_by_then_df, [categorical_column])
 
 	# Avg 
-	grouped_by_then_df = dataframe.groupBy(categorical_column).avg(numeric_column).withColumnRenamed(f"avg({numeric_column})",f"group_by_{categorical_column}_avg_{numeric_column}")
-	dataframe = dataframe.join(grouped_by_then_df, [categorical_column])
+	#grouped_by_then_df = dataframe.groupBy(categorical_column).avg(numeric_column).withColumnRenamed(f"avg({numeric_column})",f"group_by_{categorical_column}_avg_{numeric_column}")
+	#dataframe = dataframe.join(grouped_by_then_df, [categorical_column])
 
-	# Mean 
-	grouped_by_then_df = dataframe.groupBy(categorical_column).mean(numeric_column).withColumnRenamed(f"mean({numeric_column})",f"group_by_{categorical_column}_mean_{numeric_column}")
-	dataframe = dataframe.join(grouped_by_then_df, [categorical_column])
+	# Count 
+	#if f"group_by_{categorical_column}_count" not in dataframe.schema.names:
+	#	grouped_by_then_df = dataframe.groupBy(categorical_column).count().withColumnRenamed(f"count",f"group_by_{categorical_column}_count")
+	#	dataframe = dataframe.join(grouped_by_then_df, [categorical_column])
 
-	return dataframe
+	group_by_then_transformer = GroupByThenTransformer(inputCols=[categorical_column, numeric_column], outputCols=["min", "max", "avg", "count"])
+	dataframe = group_by_then_transformer.transform(dataframe)
+
+	return dataframe, group_by_then_transformer
+
+
+def create_features_column(dataframe, columns):
+	"""Create a feature column from the dataframe."""
+	assembler = VectorAssembler(
+	    inputCols=columns,
+	    outputCol="features")
+
+	dataframe = assembler.transform(dataframe)
+	return dataframe, assembler
+
+
 
