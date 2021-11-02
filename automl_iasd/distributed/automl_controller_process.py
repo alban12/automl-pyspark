80556242eec3384c99343670f3147b20e3615380
Alban Tiacoh <alban.kdf@gmail.com>
2021-11-02 14:20:23 +0100

    Add verbose

--- automl_iasd/distributed/automl_controller_process.py
+++ automl_iasd/distributed/automl_controller_process.py
@@ -48,37 +48,10 @@ def getSparkJobFinalStatus(application_id):
 def executeThread(app_name, spark_submit_cmd, error_log_dir,
         max_wait_time_job_start_s = 0):
     cmd_output = subprocess.Popen(spark_submit_cmd, shell=True,
-        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    start_time = time.time()
-    yarn_application_id = ""
-    while yarn_application_id == '' and time.time()-start_time\
-            < max_wait_time_job_start_s:
-        yarn_application_id = getYARNApplicationID(app_name)
-    print(f"Launching thread for {app_name} with {spark_submit_cmd}")
-    cmd_output.wait()
-    # if yarn_application_id == '':
-    #     raise RuntimeError("Couldn't get yarn application ID for"\
-    #         " application %s" % (app_name))
-    #     # Replace line above by the following if you do not
-    #     # want a failed task to stop the entire process:
-    #     # return False
-    # final_status = getSparkJobFinalStatus(yarn_application_id)
-    # print(final_status)
-    # log_path = "."
-    # if final_status != "SUCCEEDED":
-    #     cmd_output = subprocess.Popen(["yarn","logs", 
-    #         "-applicationId",yarn_application_id],
-    #          stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-    #          bufsize=1, universal_lines=True)
-    #     with open(log_path, "w") as f:
-    #         for line in cmd_output.stdout:
-    #             f.write(line)
-    #     print("Written log of failed task to %s" % log_path)
-    #     cmd_output.communicate()
-    #     raise RuntimeError("Task %s has not succeeded" % app_name)
-    #     # Replace line above by the following if you do not
-    #     # want a failed task to stop the entire process:
-    #     # return False
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in cmd_output.stdout:
+    	print(line)
+	cmd_output.communicate()
     return True
 
 
