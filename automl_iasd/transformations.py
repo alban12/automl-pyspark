e089fa5ca5e623e4af7427dbc09fb04f446bece7
alban12 <48073591+alban12@users.noreply.github.com>
2021-11-15 11:36:32 +0100

    Clean repository

--- automl_iasd/transformations.py
+++
@@ -1,7 +0,0 @@
-import pyspark.sql.functions as F
-
-def with_greeting(df):
-    return df.withColumn("greeting", F.lit("hello!")) 
-
-def remove_non_word_characters(col):
-    return F.regexp_replace(col, "[^\\w\\s]+", "")
